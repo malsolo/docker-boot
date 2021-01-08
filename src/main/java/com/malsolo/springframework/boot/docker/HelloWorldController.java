d882d39956fea25f35a439e2cda56aa3a7d3292a
Javier Beneito Barquero <malsolo.com@outlook.com>
2021-01-08 10:51:24 +0100

    Docker-boot: kubernetes liveness and readiness probes with actuator.

--- src/main/java/com/malsolo/springframework/boot/docker/HelloWorldController.java
+++ src/main/java/com/malsolo/springframework/boot/docker/HelloWorldController.java
@@ -1,11 +1,18 @@
 package com.malsolo.springframework.boot.docker;
 
+import lombok.AllArgsConstructor;
+import org.springframework.boot.availability.AvailabilityChangeEvent;
+import org.springframework.boot.availability.ReadinessState;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
+@AllArgsConstructor
 public class HelloWorldController {
+
+    private final ApplicationEventPublisher publisher;
 
     @GetMapping("/hello")
     public Mono<String> hello() throws InterruptedException {
@@ -13,4 +20,15 @@ public class HelloWorldController {
         return Mono.just("Hello World");
     }
 
+    @GetMapping("/up")
+    public Mono<String> up() {
+        AvailabilityChangeEvent.publish(publisher, this, ReadinessState.ACCEPTING_TRAFFIC);
+        return Mono.just("up");
+    }
+
+    @GetMapping("/down")
+    public Mono<String> down() {
+        AvailabilityChangeEvent.publish(publisher, this, ReadinessState.REFUSING_TRAFFIC);
+        return Mono.just("down");
+    }
 }
